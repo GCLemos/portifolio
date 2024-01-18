40965e6f441cff3ce49ef871ff6338b8db26b8e4
Gabriel Lemos <73192420+GCLemos@users.noreply.github.com>
2024-01-18 15:12:20 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,8 +1,33 @@
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-    anchor.addEventListener('click', function (e) {
-        e.preventDefault();
-        document.querySelector(this.getAttribute('href')).scrollIntoView({
-            behavior: "smooth"
-        });
-    });
+// Select all sections
+const sections = document.querySelectorAll('section');
+
+// Create an Intersection Observer
+const observer = new IntersectionObserver((entries) => {
+  entries.forEach(entry => {
+    const id = entry.target.getAttribute('id');
+    const anchorLink = document.querySelector(`a[href="#${id}"]`);
+
+    if (entry.isIntersecting) {
+      anchorLink.classList.add('active');
+    } else {
+      anchorLink.classList.remove('active');
+    }
+  });
+}, { threshold: 0.9 }); // Adjust the threshold according to your needs
+
+// Observe each section
+sections.forEach(section => {
+  observer.observe(section);
 });
+
+// Existing code
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+  anchor.addEventListener('click', function (e) {
+    e.preventDefault();
+    document.querySelector(this.getAttribute('href')).scrollIntoView({
+      behavior: "smooth"
+    });
+  });
+});
+
+
