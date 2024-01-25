7ace3557d9dc1ac022f861409fb14a1356bee44d
Gabriel Lemos <73192420+GCLemos@users.noreply.github.com>
2024-01-25 17:44:10 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -1,33 +1,44 @@
-// Select all sections
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: "smooth"
+        });
+    });
+});
+
 const sections = document.querySelectorAll('section');
 
-// Create an Intersection Observer
-const observer = new IntersectionObserver((entries) => {
-  entries.forEach(entry => {
-    const id = entry.target.getAttribute('id');
-    const anchorLink = document.querySelector(`a[href="#${id}"]`);
-
-    if (entry.isIntersecting) {
-      anchorLink.classList.add('active');
-    } else {
-      anchorLink.classList.remove('active');
-    }
-  });
-}, { threshold: 0.9 }); // Adjust the threshold according to your needs
-
-// Observe each section
-sections.forEach(section => {
-  observer.observe(section);
-});
-
-// Existing code
-document.querySelectorAll('a[href^="#"]').forEach(anchor => {
-  anchor.addEventListener('click', function (e) {
-    e.preventDefault();
-    document.querySelector(this.getAttribute('href')).scrollIntoView({
-      behavior: "smooth"
+window.addEventListener('scroll', () => {
+    sections.forEach(section => {
+        const icon = document.querySelector(`a[href="#${section.id}"] i`);
+        const rect = section.getBoundingClientRect();
+        if (rect.top >= 0 && rect.bottom <= window.innerHeight) {
+            icon.classList.add('active');
+        } else {
+            icon.classList.remove('active');
+        }
     });
-  });
 });
+
+
+
+window.onscroll = function() {myFunction()};
+
+// Get the navbar
+var navbar = document.getElementById("navbar");
+
+// Get the offset position of the navbar
+var sticky = navbar.offsetTop;
+
+// Add the sticky class to the navbar when you reach its scroll position. Remove "sticky" when you leave the scroll position
+function myFunction() {
+  if (window.scrollY >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
+
 
 
